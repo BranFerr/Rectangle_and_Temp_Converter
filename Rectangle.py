8224da71e873810daeb50644dc076e2c3b33ae3f
Branson Ferrell <62112844+BranFerr@users.noreply.github.com>
2021-03-19 21:56:27 -0400

    Add files via upload

---
+++ Rectangle.py
@@ -0,0 +1,9 @@
+h = input('What is the Height/Length of the Rectangle?\n')
+h = int (h)
+
+w = int (input('What is the Width of the Rectangle?\n'))
+
+x = int (h) * w
+x = str (x)
+h = str (h)
+print ('The area of a rectangle of Height/Length '+h+' and width ',w,' is '+x)
---
+++ Tempconvert.py
@@ -0,0 +1,4 @@
+h = input("'temp' in Celsius")
+h = float (h)
+hh=1.8*h+32
+print('The Celsius temperature',h,' is equivalent to Fahrenheit temp',hh)
